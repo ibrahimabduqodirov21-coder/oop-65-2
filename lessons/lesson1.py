16f170893838bd13bf044737a457f0539d32fc82
Ibrahim <forstudyy21@gmail.com>
2026-04-04 22:31:00 +0600

    Update: lesson1

--- lessons/lesson1.py
+++ lessons/lesson1.py
@@ -1,24 +1,24 @@
-# class Hero:
-#
-#     # Конструктор класса
-#     def __init__(self, name, hp, lvl):
-#         # Атрибуты класса
-#         self.name_hero = name
-#         self.hp_hero = hp
-#         self.lvl_hero = lvl
-#
-#     # Метод класса
-#     def action(self):
-#         return f"{self.name_hero} hero base action!!"
-#
-#
-# # Обьект|Экземпляр на основе класса
-# kirito = Hero("Kirito", 1000, 100)
-# asuna = Hero("Asuna", 10000, 1000)
-#
-# print(kirito.action())
-# print(asuna.action())
-#
+class Hero:
+
+    # Конструктор класса
+    def __init__(self, name, hp, lvl):
+        # Атрибуты класса
+        self.name_hero = name
+        self.hp_hero = hp
+        self.lvl_hero = lvl
+
+    # Метод класса
+    def action(self):
+        return f"{self.name_hero} hero base action!!"
+
+
+# Обьект|Экземпляр на основе класса
+kirito = Hero("Kirito", 1000, 100)
+asuna = Hero("Asuna", 10000, 1000)
+
+print(kirito.action())
+print(asuna.action())
+
 # HeroMage - just for class
 # hero_mage - ...
 
